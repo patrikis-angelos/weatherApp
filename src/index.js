3fef86cdbcf20b8c9029e43390e291796e71efe1
patrick-angelos <angelosp07@gmail.com>
2021-04-06 19:59:31 +0300

    Add function to get data from openWeatherMap

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import keys from './env'
+
+async function getWeather() {
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Athens&appid=${keys.weather_key}`, {mode: 'cors'})
+  return response.json();
+}
+
+
+const s = getWeather();
+s.then((response) => {
+  console.log(response);
+})
