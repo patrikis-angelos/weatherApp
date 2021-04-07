96a711c748a305131cb57d60f186732b77d32b91
patrick-angelos <angelosp07@gmail.com>
2021-04-07 13:01:15 +0300

    create onSubmit function

--- src/home.js
+++ src/home.js
@@ -1,5 +1,19 @@
 import * as weather from './weather';
 
+const onSubmit = (e) => {
+  e.preventDefault();
+  const city = document.querySelector('#city').value;
+  const data = weather.getWeather(city);
+  const report = weather.getInfo(data);
+  report.then((value) => {
+    createReport(value.getReport());
+  })
+}
+
+const createReport = (report) => {
+  console.log(report);
+}
+
 const createForm = () => {
   const form = document.createElement('form');
   form.classList.add('border', 'form', 'flex', 'space-between')
@@ -11,14 +25,7 @@ const createForm = () => {
   const submit = document.createElement('button');
   submit.type = 'submit';
   submit.classList.add('submit','no-border', "fas", "fa-search");
-  submit.onclick = (e) => {
-    e.preventDefault();
-    const data = weather.getWeather(cityInput.value);
-    const report = weather.getInfo(data);
-    report.then((value) => {
-      value.getReport();
-    })
-  }
+  submit.onclick = onSubmit.bind(this);
   form.appendChild(cityInput);
   form.appendChild(submit);
   return form;
--- src/weather.js
+++ src/weather.js
@@ -1,26 +1,24 @@
 const Weather = (name, country, desc, temp, humidity, speed) => {
-  const getTemp = () => temp;
-
-  const getReport = () => console.log(`${name} ${country} ${desc} ${temp} ${humidity} ${speed}`);
+  const getReport = () => [name, country, desc, temp, humidity, speed];
 
   return {getReport}
 };
 
 async function getWeather(city) {
-const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_key}`, {mode: 'cors'})
-return response.json();
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_key}`, {mode: 'cors'})
+  return response.json();
 }
 
 async function getInfo(data) {
-const info = await data;
-const report = Weather(
-  info.name,
-  info.sys.country,
-  info.weather[0].description,
-  info.main.temp,
-  info.main.humidity,
-  info.wind.speed);
-return report;
+  const info = await data;
+  const report = Weather(
+    info.name,
+    info.sys.country,
+    info.weather[0].description,
+    info.main.temp,
+    info.main.humidity,
+    info.wind.speed);
+  return report;
 }
 
 export default getWeather;
